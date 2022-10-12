ec2bf9f80dea3a91fedcfde01fc86ce256e7fdf8
shayanaqvi <shayan.naqvi@icloud.com>
2022-10-12 15:51:23 +0500

    custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(terminalii-theme terminal-theme oswald-theme tty-dark-theme euphoria-theme clarity-theme arjen-theme taming-mr-arneson-theme)))
+   '(splash-screen terminalii-theme terminal-theme oswald-theme tty-dark-theme euphoria-theme clarity-theme arjen-theme taming-mr-arneson-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
